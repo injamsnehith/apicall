1468742712adbf5114afb57091463adf45551186
INJAM SNEHITH <injam.snehith@terralogic.com>
2021-01-25 10:30:09 +0530

    apicall

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import {BrowserRouter as Router, HashRouter, Switch , Route} from "react-router-dom";
-import day6 from "./component/day6"
 import table from "./component/table"
 
 function App(){
